b9a6e7cde66e2ed16feed8ddc063db3075df65ec
Al Ricciardelli <alricciardelli@Als-MacBook-Pro.local>
2021-07-31 12:12:48 -0700

    Add unique ID to list and sale final csvs

--- database/zillow_db_schema.sql
+++ database/zillow_db_schema.sql
@@ -1,32 +1,35 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+
 CREATE TABLE "List" (
+    "region_date" varchar   NOT NULL,
     "region_id" varchar   NOT NULL,
     "size_rank" int   NOT NULL,
     "region_name" varchar   NOT NULL,
     "region_type" varchar   NOT NULL,
-    "state_name" varchar   NOT NULL,
+    "state_name" varchar,
     "date" date   NOT NULL,
-    "price" float   NOT NULL,
+    "price" float,
     CONSTRAINT "pk_List" PRIMARY KEY (
-        "region_id"
+        "region_date"
      )
 );
 
 CREATE TABLE "Sale" (
+    "region_date" varchar   NOT NULL,
     "region_id" varchar   NOT NULL,
     "size_rank" int   NOT NULL,
     "region_name" varchar   NOT NULL,
     "region_type" varchar   NOT NULL,
-    "state_name" varchar   NOT NULL,
+    "state_name" varchar,
     "date" date   NOT NULL,
-    "price" float   NOT NULL,
+    "price" float,
     CONSTRAINT "pk_Sale" PRIMARY KEY (
-        "region_id"
+        "region_date"
      )
 );
 
-ALTER TABLE "List" ADD CONSTRAINT "fk_List_region_id_date" FOREIGN KEY("region_id", "date")
-REFERENCES "Sale" ("region_id", "date");
+ALTER TABLE "List" ADD CONSTRAINT "fk_List_region_date" FOREIGN KEY("region_date")
+REFERENCES "Sale" ("region_date");
 
